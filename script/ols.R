760cb82aeb4aa8d6b7e520812cd6cfc3fb982cdd
Lydia Maher <Lydia Maher>
2016-11-04 11:54:17 -0700

    First Commit

---
+++ script/ols.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/Lydia/Documents/Stat159/proj2")
+scaled_credit <-read.csv(file = "./data/scaled_credit.csv")
+
+##OLS
+ols <-lm(Balance~., data = data.frame(scaled_credit))
+ols_coeffs <- as.numeric(ols$coefficients)[-1]
