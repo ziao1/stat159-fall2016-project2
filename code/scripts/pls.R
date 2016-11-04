650293263e84b34c819a36232ef7da010091b194
ziao1 <liuziao95@gmail.com>
2016-11-04 14:54:48 -0700

    update pls

--- code/scripts/pls.R
+++ code/scripts/pls.R
@@ -1,6 +1,24 @@
 ## Partial Least Squares Regression
 library(pls)
+#pre-modeling data processing
+credit <- read.csv("data/Credit.csv")
+credit <- credit[,-1]
 
+#dummy out categorical variables
+temp_credit <- model.matrix(Balance ~ ., data = credit)
+
+#removing column of ones, and appending Balance
+new_credit <- cbind(temp_credit[,-1], Balance = credit$Balance)
+
+#scaling and centering
+scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
+
+set.seed(5)
+train_set <- sample(c(1:400), size = 300)
+predictors = scaled_credit[,c(1:11)]
+response = scaled_credit[,c(12)]
+test=(-train_set)
+response_test=response[test]
 ## Partial Least Squares Regression
 pls_fit = plsr(Balance ~ ., data = data.frame(scaled_credit), subset = train_set, scale = FALSE, validation ="CV")
 summary(pls_fit)
