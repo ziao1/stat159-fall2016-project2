6d49a96c4c6336b903b82dd47e03de9a767285b9
ziao1 <liuziao95@gmail.com>
2016-11-04 14:54:01 -0700

    update pcr

--- code/scripts/pcr.R
+++ code/scripts/pcr.R
@@ -1,5 +1,24 @@
-#setwd("C:/Users/Lydia/Documents/Stat159/proj2")
+# PCR
 library(pls)
+#pre-modeling data processing
+credit <- read.csv("data/Credit.csv")
+credit <- credit[,-1]
+
+#dummy out categorical variables
+temp_credit <- model.matrix(Balance ~ ., data = credit)
+
+#removing column of ones, and appending Balance
+new_credit <- cbind(temp_credit[,-1], Balance = credit$Balance)
+
+#scaling and centering
+scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
+
+set.seed(5)
+train_set <- sample(c(1:400), size = 300)
+predictors = scaled_credit[,c(1:11)]
+response = scaled_credit[,c(12)]
+test=(-train_set)
+response_test=response[test]
 
 ## Principal Components Regression
 pcr_fit = pcr(Balance ~ ., data = data.frame(scaled_credit), subset = train_set, scale = FALSE, validation ="CV")
