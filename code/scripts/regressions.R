553223184e118b4367e1c153f51dba44fcc87f84
ziao1 <liuziao95@gmail.com>
2016-11-04 14:53:42 -0700

    update regressions

--- code/scripts/regressions.R
+++ code/scripts/regressions.R
@@ -1,5 +1,26 @@
 ##regression models
 library("glmnet")
+library(pls)
+#pre-modeling data processing
+credit <- read.csv("data/Credit.csv")
+credit <- credit[,-1]
+
+#dummy out categorical variables
+temp_credit <- model.matrix(Balance ~ ., data = credit)
+
+#removing column of ones, and appending Balance
+new_credit <- cbind(temp_credit[,-1], Balance = credit$Balance)
+
+#scaling and centering
+scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
+
+set.seed(5)
+train_set <- sample(c(1:400), size = 300)
+predictors = scaled_credit[,c(1:11)]
+response = scaled_credit[,c(12)]
+test=(-train_set)
+response_test=response[test]
+
 
 ##OLS
 ols <-lm(Balance~., data = data.frame(scaled_credit))
